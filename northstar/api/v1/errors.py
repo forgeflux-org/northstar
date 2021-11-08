4bf93cdbaf4924af81b87e05add8a0dbe5a2a613
realaravinth <realaravinth@batsense.net>
2021-11-08 15:06:36 +0530

    move all erros into errors module

--- northstar/api/v1/errors.py
+++ northstar/api/v1/errors.py
@@ -42,3 +42,42 @@ class Error:
         resp = jsonify(self.get_error())
         resp.status = self.__status
         return resp
+
+
+F_D_NO_REGISTERED_INTERFACES = Error(
+    errcode="F_D_NO_REGISTERED_INTERFACES",
+    error="No interfaces are registered against the queried forge",
+    status=400,
+)
+
+F_D_INTERNAL_SERVER_ERROR = Error(
+    errcode="F_D_INTERNAL_SERVER_ERROR",
+    error="Operation could not be performed due to internal errors.",
+    status=500,
+)
+
+
+F_D_EMPTY_FORGE_LIST = Error(
+    errcode="F_D_EMPTY_FORGE_LIST",
+    error="The forge list submitted is empty",
+    status=400,
+)
+
+F_D_INTERFACE_UNREACHABLE = Error(
+    errcode="F_D_INTERFACE_UNREACHABLE",
+    error="The interface was unreachable with the publicly accessible URL provided",
+    status=503,
+)
+
+F_D_INVALID_PAYLOAD = Error(
+    errcode="F_D_INVALID_PAYLOAD",
+    error="Please submit valid payload",
+    status=400,
+)
+
+
+F_D_NOT_URL = Error(
+    errcode="F_D_NOT_URL",
+    error="Please submit valid URL",
+    status=400,
+)
