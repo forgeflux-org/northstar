21db33422feda4d0447b01e5b9ea204de097ba5d
realaravinth <realaravinth@batsense.net>
2022-02-03 17:03:29 +0530

    chore: init db module

--- northstar/db/__init__.py
+++ northstar/db/__init__.py
@@ -12,31 +12,4 @@
 # GNU Affero General Public License for more details.
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
-import sqlite3
-
-import pytest
-from northstar.db import get_db
-
-
-def test_get_close_db(app):
-    with app.app_context():
-        db = get_db()
-        assert db is get_db()
-
-    with pytest.raises(sqlite3.ProgrammingError) as e:
-        db.execute("SELECT 1")
-
-    assert "closed" in str(e.value)
-
-
-def test_init_db_command(runner, monkeypatch):
-    class Recorder(object):
-        called = False
-
-    def fake_init_db():
-        Recorder.called = True
-
-    monkeypatch.setattr("northstar.db.conn.init_db", fake_init_db)
-    result = runner.invoke(args=["migrate"])
-    assert "applied" in result.output
-    assert Recorder.called
+from .conn import get_db, init_app, init_db
